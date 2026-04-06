5f3ad193647a295d9693bc1eefa503aed86c3a94
BOSSECODE <eduarda_b_miranda@estudante.sesisenai.org.br>
2026-04-06 09:11:43 -0300

    ADD- parte de html finalizada

---
+++ script.js
@@ -0,0 +1,60 @@
+const form = document.getElementById("formdoacao");
+
+
+form.addEventListener("submit", function(e) {
+
+    e.preventDefault();
+
+
+    let nome = document.getElementById("nome").value;
+    let idade = document.getElementById("idade").value;
+    let peso = document.getElementById("peso").value;
+    let email = document.getElementById("email").value;
+    let telefone = document.getElementById("telefone").value;
+    let cidade = document.getElementById("cidade").value;
+    let estado = document.getElementById("estado").value;
+    let tipo_sanguineo = document.getElementById("tipo_sanguineo").value;
+
+
+   
+    if (nome.length < 3) {
+        alert("Nome inválido");
+        return;
+    }
+
+
+    if (idade < 16) {
+        alert("Você precisa ter 16 anos ou mais");
+        return;
+    }
+
+   
+    if (!email.includes("@")) {
+        alert("Email inválido");
+        return;
+    }
+
+    
+    if (telefone.length < 8) {
+        alert("Telefone inválido");
+        return;
+    }
+
+    
+    if (cidade.trim() === "") {
+        alert("Cidade é obrigatória");
+        return;
+    }
+
+    if (estado.trim() === "") {
+        alert("Estado é obrigatório");
+        return;
+    }
+    
+    if (peso < 50) {
+        alert("Peso insuficiente para doação");
+        return;
+    }
+
+    alert("Cadastro realizado com sucesso!");
+});
